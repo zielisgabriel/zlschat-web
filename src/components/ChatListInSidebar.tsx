2c8aa4d0654eef0c5bc274a0c5eaad5ed29a63dc
zielisgabriel <josegabrielas0586@email.com>
2025-08-02 15:33:53 -0300

    refactor: Separate ChatList of Sidebar

---
+++ src/components/ChatListInSidebar.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { ChatRoom } from "@/@types/ChatRoom";
+import { api } from "@/lib/api";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "sonner";
+import { SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { ApplicationContext } from "@/contexts/ApplicationContext";
+
+interface ChatListInSidebarProps {
+    onLoadChatRoom: (chatId: string) => Promise<void>;
+}
+
+export function ChatListInSidebar(props: ChatListInSidebarProps) {
+    const [chats, setChats] = useState<ChatRoom[]>([]);
+    const {profile} = useContext(ApplicationContext);
+
+    async function onFetchChats() {
+        try {
+            const response = await api.get("/api/chat/list");
+            if (response.status !== 200) {
+                throw new Error(response.data.message);
+            }
+            setChats(response.data);
+        } catch (error: any) {
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        onFetchChats();
+    }, []);
+
+    if (chats.length === 0) {
+        return (
+            <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                    <button className="cursor-pointer" onClick={onFetchChats}>
+                        Nenhum chat encontrado
+                    </button>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        );
+    }
+
+    return (
+        chats.map((chat, index) => (
+            <SidebarMenuItem key={index}>
+                <SidebarMenuButton asChild>
+                    <button className="cursor-pointer" onClick={async () => await props.onLoadChatRoom(chat.id!)}>
+                        <Avatar>
+                            <AvatarFallback>
+                                {
+                                    chat.usersInChat.find((user) => user !== profile?.username)?.toUpperCase().charAt(0)
+                                }
+                            </AvatarFallback>
+                        </Avatar>
+                        <span>
+                            {
+                                chat.chatRoomType === "PRIVATE" ? (
+                                    chat.name ? (
+                                        chat.name
+                                    ) : (
+                                        chat.usersInChat.find((user) => user !== profile?.username)
+                                    )
+                                ) : (
+                                    chat.name ? (
+                                        chat.name
+                                    ) : (
+                                        chat.usersInChat.join(", ")
+                                    )
+                                )
+                            }
+                        </span>
+                    </button>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        ))
+    );
+}
