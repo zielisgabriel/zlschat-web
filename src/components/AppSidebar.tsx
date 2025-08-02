c14070a58dad98de67500d6c5a9741113804818f
zielisgabriel <josegabrielas0586@email.com>
2025-08-02 15:28:41 -0300

    refactor

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,189 +1,41 @@
 "use client";
 
-import { CheckIcon, ChevronDown, LogOutIcon, PlusIcon } from "lucide-react"
+import { ChevronDown, LogOutIcon } from "lucide-react"
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { ApplicationContext } from "@/contexts/ApplicationContext";
 import { useRouter } from "next/navigation";
 import { api } from "@/lib/api";
-import { Input } from "./ui/input";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem } from "./ui/form";
-import { Button } from "./ui/button";
-import { toast } from "sonner";
-import { ChatRoom } from "@/@types/ChatRoom";
-
-const searchFriendOrProfileSchema = z.object({
-    username: z.string(),
-});
-
-type SearchFriendOrProfileType = z.infer<typeof searchFriendOrProfileSchema>;
+import { ChatListInSidebar } from "./ChatListInSidebar";
+import { Separator } from "./ui/separator";
+import { SearchOtherUsersInSidebar } from "./SearchOtherUsersInSidebar";
 
 export function AppSidebar() {
     const { profile, onLoadChatAndMessagesInChat } = useContext(ApplicationContext);
-    const [chats, setChats] = useState<ChatRoom[]>([]);
-    const [otherUsers, setOtherUsers] = useState<User[]>([]);
     const router = useRouter();
-    const form = useForm<SearchFriendOrProfileType>({
-        resolver: zodResolver(searchFriendOrProfileSchema),
-        defaultValues: {
-            username: ""
-        }
-    });
-
-    const searchChatsOrProfileWatch = form.watch("username");
 
     async function onLogout() {
         await api.post("/api/user/logout");
         router.push("/login");
     }
 
-    async function onFetchMyChats() {
-        try {
-            const response = await api.get("/api/chat/list");
-            if (response.status !== 200) {
-                throw new Error(response.data.message);
-            }
-            setChats(response.data);
-        } catch (error: any) {
-            toast.error(error.message);
-        }
-    }
-
-    async function onFetchOtherUsers(username: string) {
-        try {
-            const response = await api.get(`/api/user/find/${username}`);
-            if (response.status !== 200) {
-                throw new Error(response.data.message);
-            }
-            setOtherUsers(response.data);
-        } catch (error: any) {
-            toast.error(error.message);
-        }
-    }
-
-    async function onRequestFriendship(username: string) {
-        try {
-            const response = await api.post(`/api/user/friends/request/${username}`)
-            if (response.status !== 200) {
-                throw new Error(response.data.message);
-            }
-            toast.success("Pedido de amizade enviado com sucesso!");
-        } catch (error: any) {
-            toast.error(error.message);
-        }
-    }
-
-    useEffect(() => {
-        onFetchMyChats();
-    }, []);
-    
-    useEffect(() => {
-        if (searchChatsOrProfileWatch.length > 0) {
-            const timeout = setTimeout(async () => {
-                await onFetchOtherUsers(searchChatsOrProfileWatch);
-            }, 1500);
-
-            return () => clearTimeout(timeout);
-        }
-    }, [searchChatsOrProfileWatch]);
-
     return (
         <Sidebar collapsible="none" className="h-screen">
             <SidebarContent>
                 <SidebarGroup>
                     <SidebarGroupLabel>
-                        Zls Chat
+                        Lista de chats
                     </SidebarGroupLabel>
 
                     <SidebarGroupContent>
-                        <Form {...form}>
-                            <form>
-                                <FormField
-                                    control={form.control}
-                                    name="username"
-                                    render={({ field }) => {
-                                        return (
-                                            <FormItem className="mb-2">
-                                                <FormControl>
-                                                    <Input type="text" placeholder="Procurar" {...field} />
-                                                </FormControl>
-                                            </FormItem>
-                                        )
-                                    }}
-                                />
-                            </form>
-                        </Form>
-
                         <SidebarMenu>
-                            {searchChatsOrProfileWatch && otherUsers ? (
-                                otherUsers.map((otherUser, index) => (
-                                    <SidebarMenuItem key={index}>
-                                        <div className="flex justify-between items-center">
-                                            <div className="flex gap-2 items-center px-2">
-                                                <Avatar>
-                                                    <AvatarFallback>
-                                                        {otherUser.username.toUpperCase().charAt(0)}
-                                                    </AvatarFallback>
-                                                </Avatar>
-                                                <span>
-                                                    {otherUser.username}
-                                                </span>
-                                            </div>
-
-                                            {
-                                                profile?.friends.includes(otherUser.username) ? (
-                                                    <CheckIcon className="w-4 h-4" />
-                                                ) : (
-                                                    <Button
-                                                        onClick={() => onRequestFriendship(otherUser.username)}
-                                                        className="w-7 h-7 rounded-full cursor-pointer"
-                                                    >
-                                                        <PlusIcon className="w-3 h-3" />
-                                                    </Button>
-                                                )
-                                            }
-                                        </div>
-                                    </SidebarMenuItem>
-                                ))
-                            ) : (
-                                chats.map((chat, index) => (
-                                    <SidebarMenuItem key={index}>
-                                        <SidebarMenuButton asChild>
-                                            <button className="cursor-pointer" onClick={async () => await onLoadChatAndMessagesInChat(chat.id!)}>
-                                                <Avatar>
-                                                    <AvatarFallback>
-                                                        {
-                                                            chat.usersInChat.find((user) => user !== profile?.username)?.toUpperCase().charAt(0)
-                                                        }
-                                                    </AvatarFallback>
-                                                </Avatar>
-                                                <span>
-                                                    {
-                                                        chat.chatRoomType === "PRIVATE" ? (
-                                                            chat.name ? (
-                                                                chat.name
-                                                            ) : (
-                                                                chat.usersInChat.find((user) => user !== profile?.username)
-                                                            )
-                                                        ) : (
-                                                            chat.name ? (
-                                                                chat.name
-                                                            ) : (
-                                                                chat.usersInChat.join(", ")
-                                                            )
-                                                        )
-                                                    }
-                                                </span>
-                                            </button>
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))
-                            )}
+                            <SearchOtherUsersInSidebar />
+                            <Separator className="my-2" />
+                            <ChatListInSidebar
+                                onLoadChatRoom={async (chatId) => await onLoadChatAndMessagesInChat(chatId)}
+                            />
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
