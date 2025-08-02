8dfe56161e5466354bd7dd4df3e34484c5176640
zielisgabriel <josegabrielas0586@email.com>
2025-08-02 15:34:36 -0300

    refactor: SearchOtherUsers component separate of Sidebar component

---
+++ src/components/SearchOtherUsersInSidebar.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import { api } from "@/lib/api";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+import { SidebarMenuItem } from "./ui/sidebar";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { ApplicationContext } from "@/contexts/ApplicationContext";
+import { CheckIcon, PlusIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
+import { Input } from "./ui/input";
+import { Spinner } from "./ui/shadcn-io/spinner";
+
+const searchFriendOrProfileSchema = z.object({
+    username: z.string(),
+});
+
+type SearchFriendOrProfileType = z.infer<typeof searchFriendOrProfileSchema>;
+
+export function SearchOtherUsersInSidebar() {
+    const {profile} = useContext(ApplicationContext);
+    const [otherUsers, setOtherUsers] = useState<User[]>([]);
+    const [isLoadingOtherUsers, setIsLoadingOtherUsers] = useState<boolean>(false);
+    const form = useForm<SearchFriendOrProfileType>({
+        resolver: zodResolver(searchFriendOrProfileSchema),
+        defaultValues: {
+            username: ""
+        }
+    });
+
+    const searchChatsOrProfileWatch = form.watch("username");
+
+    async function onFetchOtherUsers(username: string) {
+        try {
+            setIsLoadingOtherUsers(true);
+            const response = await api.get(`/api/user/find/${username}`);
+            if (response.status !== 200) {
+                throw new Error(response.data.message);
+            }
+            setOtherUsers(response.data);
+            setIsLoadingOtherUsers(false);
+        } catch (error: any) {
+            setIsLoadingOtherUsers(false);
+            toast.error(error.message);
+        }
+    }
+
+    async function onRequestFriendship(username: string) {
+        try {
+            const response = await api.post(`/api/user/friends/request/${username}`)
+            if (response.status !== 200) {
+                throw new Error(response.data.message);
+            }
+            toast.success("Pedido de amizade enviado com sucesso!");
+        } catch (error: any) {
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        setIsLoadingOtherUsers(true);
+        if (searchChatsOrProfileWatch.length > 0) {
+            const timeout = setTimeout(async () => {
+                await onFetchOtherUsers(searchChatsOrProfileWatch);
+            }, 1000);
+
+            return () => clearTimeout(timeout);
+        }
+    }, [searchChatsOrProfileWatch]);
+
+    return (
+        <>
+            <Form {...form}>
+                <form>
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => {
+                            return (
+                                <FormItem className="mb-2">
+                                    <FormControl>
+                                        <Input
+                                            type="text"
+                                            placeholder="Buscar usuário..."
+                                            autoComplete="off"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )
+                        }}
+                    />
+                </form>
+            </Form>
+
+            {
+                searchChatsOrProfileWatch && (
+                    isLoadingOtherUsers ? (
+                        <div className="flex items-center justify-center">
+                            <Spinner className="w-4 h-4" />
+                        </div>
+                    ) : (
+                        otherUsers.length > 0 ? (
+                            otherUsers.map((otherUser, index) => (
+                                <SidebarMenuItem key={index}>
+                                    <div className="flex justify-between items-center">
+                                        <div className="flex gap-2 items-center px-2">
+                                            <Avatar>
+                                                <AvatarFallback>
+                                                    {otherUser.username.toUpperCase().charAt(0)}
+                                                </AvatarFallback>
+                                            </Avatar>
+                                            <span>
+                                                {otherUser.username}
+                                            </span>
+                                        </div>
+
+                                        {
+                                            profile?.friends.includes(otherUser.username) ? (
+                                                <CheckIcon className="w-4 h-4" />
+                                            ) : (
+                                                <Button
+                                                    onClick={() => onRequestFriendship(otherUser.username)}
+                                                    className="w-7 h-7 rounded-full cursor-pointer"
+                                                >
+                                                    <PlusIcon className="w-3 h-3" />
+                                                </Button>
+                                            )
+                                        }
+                                    </div>
+                                </SidebarMenuItem>
+                            ))
+                        ) : (
+                            <div className="flex items-center justify-center">
+                                <span className="text-sm text-muted-foreground">Nenhum usuário encontrado</span>
+                            </div>
+                        )
+                    )
+                )
+            }
+        </>
+    );
+}
