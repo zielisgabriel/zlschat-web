fa93566a5c34fe9319b325db42f5be9472bdc391
zielisgabriel <josegabrielas0586@email.com>
2025-08-29 10:20:03 -0300

    feat/refactor: tempId added and delete "DELIVERED" status

--- src/@types/Message.d.ts
+++ src/@types/Message.d.ts
@@ -1,8 +1,10 @@
 export interface Message {
     id?: string;
+    tempId: string;
     chatRoomId: string;
     senderUsername: string;
     receiverUsername: string;
     content: string;
+    status: "SENDING" | "SENT" | "READ"
     sendAt?: Date;
 }
